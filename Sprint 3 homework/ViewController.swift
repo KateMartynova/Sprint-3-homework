bd82170427dfafcadd37eb6987a760a37958d826
KateMartynova <mart-eka@yandex.ru>
2023-06-28 13:30:07 +0300

    выполнено усложненное домашнее задание

--- Sprint 3 homework/ViewController.swift
+++ Sprint 3 homework/ViewController.swift
@@ -8,7 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var StatsUITextView: UITextView!
+    
+    var number: Int = 0
+    
+    func printMyDate() -> String {
+        let mytime = Date();
+        let dateFormatter = DateFormatter();
+        dateFormatter.timeStyle = .short
+        dateFormatter.dateStyle = .short
+        return dateFormatter.string(from: mytime)
+    }
+    
+    
+    @IBOutlet weak var counter: UILabel!
+    
+    @IBAction func counterPlusUIButton(_ sender: Any) {
+        number += 1
+        counter.text = "Значение счетчика: \(number)"
+        StatsUITextView.text += "\n\(printMyDate()): значение изменено на +1"
+    }
+    
+    @IBAction func counterMinusUIButton(_ sender: Any) {
+        if number > 0 {
+            number -= 1
+            StatsUITextView.text += "\n\(printMyDate()): значение изменено на -1"
+        } else {
+            number = 0
+            StatsUITextView.text += "\n\(printMyDate()): попытка уменьшить значение счётчика ниже 0"
+        }
+        counter.text = "Значение счетчика: \(number)"
+    }
+    
+    @IBAction func counterZeroUIButton(_ sender: Any) {
+        number = 0
+        counter.text = "Значение счетчика: \(number)"
+        StatsUITextView.text += "\n\(printMyDate()): значение сброшено"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
